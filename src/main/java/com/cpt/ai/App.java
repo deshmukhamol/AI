81d638a08194ac3b6cf027551243fae57ab32a3f
Amol <deshmukh.amol@gmail.com>
2016-05-12 21:30:03 -0400

    First commit

---
+++ src/main/java/com/cpt/ai/App.java
@@ -0,0 +1,18 @@
+package com.cpt.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void guessNumber(int start,int end,int guessNo)
+    {
+    	
+    }
+}
